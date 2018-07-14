416df3de12736d383f3a6727f36f2507beaf803f
Belve D Marks <onodric@yahoo.com>
2018-07-14 16:28:40 -0500

    Lint changes

--- app/config/core/coreConfig.ts
+++ app/config/core/coreConfig.ts
@@ -1,11 +1,11 @@
-import { ILocationProvider } from 'angular';
-import { UrlRouter } from '@uirouter/core/lib/url/urlRouter';
-// import {} from '@uirouter/angularjs';
+import {ILocationProvider} from 'angular';
+import {UrlRouter} from '@uirouter/core/lib/url/urlRouter';
+// Import {} from '@uirouter/angularjs';
 
 export function config($urlRouterProvider: UrlRouter, $locationProvider: ILocationProvider) {
-    'ngInject'; // needed when directly exporting a class or function
-    if (ON_PROD) {
-        $locationProvider.html5Mode(true);
-    }
-    $urlRouterProvider.otherwise('/');
+  'ngInject'; // Needed when directly exporting a class or function
+  if (ON_PROD) {
+    $locationProvider.html5Mode(true);
+  }
+  $urlRouterProvider.otherwise('/');
 }
--- app/filters/capitalize.ts
+++ app/filters/capitalize.ts
@@ -6,21 +6,21 @@
  */
 
 export function capitalize() {
-    return (input, tolower, each) => {
-        if (input) {
-            if (tolower) {
-                if(each){
-                    return input
-                    .split(/(\s|-)/g)
-                    .filter(word => word.length)
-                    .map(word => (word[0].toUpperCase() + word.toLowerCase().slice(1)))
-                    .join('');
-                } else {
-                    return input[0].toUpperCase() + input.toLowerCase().slice(1);
-                }
-            } else {
-                return input[0].toUpperCase() + input.slice(1);
-            }
+  return (input, tolower, each) => {
+    if (input) {
+      if (tolower) {
+        if (each) {
+          return input
+            .split(/(\s|-)/g)
+            .filter(word => word.length)
+            .map(word => word[0].toUpperCase() + word.toLowerCase().slice(1))
+            .join('');
+        } else {
+          return input[0].toUpperCase() + input.toLowerCase().slice(1);
         }
+      } else {
+        return input[0].toUpperCase() + input.slice(1);
+      }
     }
+  };
 }
